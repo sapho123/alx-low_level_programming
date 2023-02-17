7bfddbf721f5324f4227a8874ced9475f4d9e25e
Saphokazi <csaphokazi@gmail.com>
2023-02-17 07:40:50 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include < stdlib.h>
+/**
+ * main - main block
+ * A program that prints the alphabetic
+ * in lower case,
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
