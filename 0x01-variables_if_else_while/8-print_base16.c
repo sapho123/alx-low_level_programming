c47aa7401627d19a142a66a0f552b897f69d669a
Saphokazi <csaphokazi@gmail.com>
2023-02-17 08:20:21 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: A program that prints all the numbers
+ * of base 16 in lowercase, followed by new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch - '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
