92a27eac51fa540e3ea960a68fbbb855febcae42
Saphokazi <csaphokazi@gmail.com>
2023-02-21 17:35:53 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -9,13 +9,13 @@ int main(void)
 {
     int r;
 
-    r =__isalpha('H');
+    r = _isalpha('H');
     _putchar(r + '0');
-    r = __isalpha('o');
+    r = _isalpha('o');
     _putchar(r + '0');
-    r = __isalpha(108);
+    r = _isalpha(108);
     _putchar(r + '0');
-    r = __isalpha(';');
+    r = _isalpha(';');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
